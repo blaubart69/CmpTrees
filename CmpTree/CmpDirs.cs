28d84a12872d3082edb69424cee5c283a0e01601
Bernhard Spindler <bernhard.spindler@unicredit.at>
2019-09-07 07:40:53 +0200

    catch exception when the sortorder is not given and write it out. also give the hint to use sorts/sortt switches for sorting

--- CmpTree/CmpDirs.cs
+++ CmpTree/CmpDirs.cs
@@ -30,49 +30,56 @@ namespace CmpTrees
             {
                 sortedItemsSrc = sortedItemsSrc.OrderBy(keySelector: k => k.cFileName, comparer: StringComparer.OrdinalIgnoreCase);
             }
-
-            Spi.Data.DiffSortedLists.Run<Win32.FIND_DATA>(
-                ListA: sortedItemsTrg,
-                ListB: sortedItemsSrc,
-                checkSortOrder: true,
-                ReportSameSame: true,
-                OnCompared: (state,a,b) =>
-                {
-                    DiffCallback(state, b, a);
-                },
-                KeyComparer: (a,b) =>
-                {
-                    int cmp = String.Compare(a.cFileName, b.cFileName, StringComparison.OrdinalIgnoreCase);
-                    if (cmp == 0)
+            try
+            {
+                Spi.Data.DiffSortedLists.Run<Win32.FIND_DATA>(
+                    ListA: sortedItemsTrg,
+                    ListB: sortedItemsSrc,
+                    checkSortOrder: true,
+                    ReportSameSame: true,
+                    OnCompared: (state, a, b) =>
                     {
-                        bool aIsDir = Spi.Misc.IsDirectory(a.dwFileAttributes);
-                        bool bIsDir = Spi.Misc.IsDirectory(b.dwFileAttributes);
+                        DiffCallback(state, b, a);
+                    },
+                    KeyComparer: (a, b) =>
+                    {
+                        int cmp = String.Compare(a.cFileName, b.cFileName, StringComparison.OrdinalIgnoreCase);
+                        if (cmp == 0)
+                        {
+                            bool aIsDir = Spi.Misc.IsDirectory(a.dwFileAttributes);
+                            bool bIsDir = Spi.Misc.IsDirectory(b.dwFileAttributes);
 
-                        cmp = (aIsDir == bIsDir)
-                            ?  0    // two directories OR two files --> same name --> return 0 
-                            : -1;   // one dir AND one file         --> same name --> return -1 to represent the difference
+                            cmp = (aIsDir == bIsDir)
+                                ? 0    // two directories OR two files --> same name --> return 0 
+                                : -1;  // one dir AND one file         --> same name --> return -1 to represent the difference
                     }
-                    return cmp;
-                },
-                AttributeComparer: (a,b) =>
-                {
-                    if ( Misc.IsDirectory(a) && Misc.IsDirectory(b) )
+                        return cmp;
+                    },
+                    AttributeComparer: (a, b) =>
                     {
+                        if (Misc.IsDirectory(a) && Misc.IsDirectory(b))
+                        {
+                            return 0;
+                        }
+
+                        long cmp;
+                        if ((cmp = (long)(a.FileSize - b.FileSize)) != 0)
+                        {
+                            return (int)cmp;
+                        }
+                        if ((cmp = CmpFileTimes(a.ftLastWriteTime, b.ftLastWriteTime)) != 0)
+                        {
+                            return (int)cmp;
+                        }
+
                         return 0;
-                    }
-
-                    long cmp;
-                    if ((cmp = (long)(a.FileSize - b.FileSize)) != 0)
-                    {
-                        return (int)cmp;
-                    }
-                    if ((cmp = CmpFileTimes(a.ftLastWriteTime, b.ftLastWriteTime)) != 0)
-                    {
-                        return (int)cmp;
-                    }
-
-                    return 0;
-                });
+                    });
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                Console.Error.WriteLine("HINT: try using --sorts/--sortt to get the directories sorted for comparing.");
+            }
         }
         public static long CmpFileTimes(System.Runtime.InteropServices.ComTypes.FILETIME a, System.Runtime.InteropServices.ComTypes.FILETIME b)
         {
